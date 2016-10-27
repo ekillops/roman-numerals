d8f564995b069c36aac38e44708a2bd5493c4fef
Erik Killops and Steve Burton <erik@>
2016-10-27 13:32:00 -0700

    add error messages for invalid inputs and updates README file

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 var converterLoop = function(number) {
   var numeralLists = [{one:"I", five:"V", ten:"X"}, {one:"X", five:"L", ten:"C"}, {one:"C", five:"D", ten:"M"}, {one:"M", five:"-", ten:"-"}];
-
+  //Break the number into an array and reverse it
   var number = number.split('').reverse();
-
+  //Loop through the array calls the digitConverter function and passes in the numeralList.
   for (var index = 0; index < number.length; index += 1) {
     number[index] = digitConverter(number[index], numeralLists[index]);
   };
-
+  //Reverses the number to its original form and joins it.
   return number.reverse().join('');
 };
 
 
 var digitConverter = function(inNumber, numeralList) {
-
+// Pass in a number and a numeral list corresponding to the 10's place.
   var one = numeralList.one;
   var five = numeralList.five;
   var ten = numeralList.ten;
   var number = parseInt(inNumber);
-
+// Convert a number between 1 and 9 to numerals using numeral list symbols.
     if (number <= 3) {
       number = one.repeat(number);
     } else if (number === 4) {
@@ -31,6 +31,19 @@ var digitConverter = function(inNumber, numeralList) {
 return number;
 };
 
+var inputCheck = function(input) {
+  // Check for valid input and ouput message for invalid, else run converterLoop.
+  var output = "";
+  if (!parseInt(input) || input.length > 4) {
+    output = "Please enter a valid number without commas!";
+  } else if (input <= 0 || input > 3999) {
+    output = "Please enter a number between 1 and 3999!";
+  } else {
+    output = converterLoop(input);
+  }
+  return output;
+};
+
 $(document).ready(function(){
 
 
@@ -38,7 +51,7 @@ $(document).ready(function(){
     event.preventDefault();
     var userNumber = $("#numberInput").val();
 
-    var result = converterLoop(userNumber);
+    var result = inputCheck(userNumber);
     $("#numeralResult").text(result);
 
   });
