a311f63302af400784a1fecd594b2b94277c9e93
Erik Killops and Steve Burton <steve@>
2016-10-27 11:33:02 -0700

    final javascript to subtract

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,46 @@
-var digitConverter = function(number) {
-  var numeralList = {one:"I", five:"V", ten:"X"}; // = numeralList
+var converterLoop = function(number) {
+  var numeralLists = [{one:"I", five:"V", ten:"X"}, {one:"X", five:"L", ten:"C"}, {one:"C", five:"D", ten:"M"}, {one:"M", five:"-", ten:"-"}];
+
+  var number = number.split('').reverse();
+
+  for (var index = 0; index < number.length; index += 1) {
+    number[index] = digitConverter(number[index], numeralLists[index]);
+  };
+
+  return number.reverse().join('');
+};
+
+
+var digitConverter = function(inNumber, numeralList) {
+
   var one = numeralList.one;
   var five = numeralList.five;
   var ten = numeralList.ten;
+  var number = parseInt(inNumber);
 
     if (number <= 3) {
-      return (number * one);
+      number = one.repeat(number);
     } else if (number === 4) {
-      return (one + five);
+      number = one + five;
     } else if (number >= 5 && number < 9) {
-      return (five + (one.repeat(number - 5)));
+      number = five + (one.repeat(number - 5));
     } else if (number === 9) {
-      return (one + ten);
+      number = one + ten;
     }
-}; 
 
-
-
-
-
-
-
-
+return number;
+};
 
 $(document).ready(function(){
 
 
-  $("#convertButton").click(function(event){
-    console.log("test");
-    var userNumber = parseInt($("#numberInput").val());
+  $("#inputBox").submit(function(event){
+    event.preventDefault();
+    var userNumber = $("#numberInput").val();
 
-
-    var result = digitConverter(userNumber);
+    var result = converterLoop(userNumber);
     $("#numeralResult").text(result);
+
   });
 
 });
